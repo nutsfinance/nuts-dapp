fe476638b8881f365214cc7daba39c605c47eb9f
Shengda <dingshengda@gmail.com>
2020-01-10 16:23:54 -0800

    Add validation to withdraw

--- src/app/instrument/wallet/wallet-withdraw/wallet-withdraw.component.ts
+++ src/app/instrument/wallet/wallet-withdraw/wallet-withdraw.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { NutsPlatformService } from '../../../common/web3/nuts-platform.service';
+import { FormControl, NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-wallet-withdraw',
@@ -11,26 +12,51 @@ export class WalletWithdrawComponent implements OnInit {
   @Input() private instrument: string;
   private selectedToken = 'ETH';
   private instrumentEscrowBalance: number;
-  private amount: number;
+  private amountControl: FormControl;
+  private withdrawForm: FormGroup;
 
   constructor(private nutsPlatformService: NutsPlatformService) { }
 
   ngOnInit() {
+    this.amountControl = new FormControl('', this.validBalance.bind(this));
+    this.withdrawForm = new FormGroup({amount: this.amountControl});
   }
 
   onTokenSelected(token: string) {
-    this.amount = 0;
+    this.amountControl.reset();
     this.selectedToken = token;
   }
 
-  async withdraw() {
-    console.log(this.selectedToken);
-    if (this.selectedToken === 'ETH') {
-      await this.nutsPlatformService.withdrawETH(this.instrument, this.amount);
-    } else {
-      await this.nutsPlatformService.withdrawToken(this.instrument, this.selectedToken, this.amount);
+  setMaxAmount() {
+    this.withdrawForm.patchValue({amount: this.instrumentEscrowBalance});
+  }
+
+  async withdraw(form: NgForm) {
+    console.log(this.withdrawForm);
+    if (!this.withdrawForm.valid) {
+      return;
     }
-    this.amount = 0;
+    if (this.selectedToken === 'ETH') {
+      await this.nutsPlatformService.withdrawETH(this.instrument, this.amountControl.value);
+    } else {
+      await this.nutsPlatformService.withdrawToken(this.instrument, this.selectedToken, this.amountControl.value);
+    }
+    form.resetForm();
     this.nutsPlatformService.balanceUpdatedSubject.next(this.selectedToken);
+  }
+
+  validBalance(control: FormControl): {[s: string]: boolean} {
+    if (!control.value) {
+      return {'required': true};
+    }
+    if (this.instrumentEscrowBalance < Number(control.value)) {
+      return {'insufficientBalance': true};
+    }
+    if (Number(control.value) <= 0) {
+      return {'nonPositiveAmount': true};
+    }
+    if (this.selectedToken !== 'ETH' && !/^[1-9][0-9]*$/.test(control.value)) {
+      return {'nonIntegerAmount': true};
+    }
   }
 }
