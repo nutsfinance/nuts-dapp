040b9151c9921d00823ca499d98d2d75713ac660
Shengda <dingshengda@gmail.com>
2020-04-04 01:13:38 -0700

    Borrowing engage

--- src/app/instrument/borrowing/borrowing-engage/borrowing-engage.component.ts
+++ src/app/instrument/borrowing/borrowing-engage/borrowing-engage.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, NgZone } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NutsPlatformService } from '../../../common/web3/nuts-platform.service';
+import { BorrowingIssuanceModel } from 'src/app/common/model/borrowing-issuance.model';
+import { InstrumentService } from 'src/app/common/web3/instrument.service';
 
 @Component({
   selector: 'app-borrowing-engage',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./borrowing-engage.component.scss']
 })
 export class BorrowingEngageComponent implements OnInit {
+  public currentAccount: string;
+  public issuances: BorrowingIssuanceModel[] = [];
+  
+  private accountUpdatedSubscription: Subscription;
+  private borrowingIssuancesUpdatedSubscription: Subscription;
 
-  constructor() { }
+  constructor(private nutsPlatformService: NutsPlatformService, private instrumentService: InstrumentService,
+    private zone: NgZone) { }
 
   ngOnInit() {
+    this.currentAccount = this.nutsPlatformService.currentAccount;
+    // Filters on maker and taker
+    this.updateBorrowingIssuances();
+    this.borrowingIssuancesUpdatedSubscription = this.instrumentService.borrowingIssuancesUpdatedSubject.subscribe(_ => {
+      this.updateBorrowingIssuances();
+    });
+    this.accountUpdatedSubscription = this.nutsPlatformService.currentAccountSubject.subscribe(_ => {
+      this.currentAccount = this.nutsPlatformService.currentAccount;
+      this.updateBorrowingIssuances();
+    });
+  }
+
+  ngOnDestroy() {
+    this.borrowingIssuancesUpdatedSubscription.unsubscribe();
+    this.accountUpdatedSubscription.unsubscribe();
+  }
+
+  updateBorrowingIssuances() {
+    this.zone.run(() => {
+      console.log('Engage: Borrowing issuance updated', this.instrumentService.borrowingIssuances.length);
+      console.log(this.instrumentService.borrowingIssuances.map(issuance => issuance.issuanceId));
+      const borrowingIssuances = this.instrumentService.borrowingIssuances.filter(issuance => {
+        // Issuances in Engageable state and the maker is not current user.
+        return issuance.state === 2 && issuance.makerAddress.toLowerCase() !== this.currentAccount.toLowerCase();
+      });
+      console.log('Engage: Filtered issuance', borrowingIssuances.length);
+      console.log(borrowingIssuances.map(issuance => issuance.issuanceId));
+      this.issuances = borrowingIssuances;
+    });
   }
 
 }
