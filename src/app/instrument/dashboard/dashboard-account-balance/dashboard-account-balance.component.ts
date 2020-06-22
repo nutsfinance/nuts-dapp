937eddbfb725eae350827c000ff814d470c66b5d
Shengda <dingshengda@gmail.com>
2020-06-22 11:33:18 -0700

    Fix account balance

--- src/app/instrument/dashboard/dashboard-account-balance/dashboard-account-balance.component.ts
+++ src/app/instrument/dashboard/dashboard-account-balance/dashboard-account-balance.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { MultiDataSet, Label } from 'ng2-charts';
 
 import { environment } from 'src/environments/environment';
-import { AccountBalanceService, AccountBalances } from 'src/app/common/web3/account-balance.service';
 import { Subscription } from 'rxjs';
 import { PriceOracleService } from 'src/app/common/web3/price-oracle.service';
-import { NutsPlatformService, USD_ADDRESS, CNY_ADDRESS } from 'src/app/common/web3/nuts-platform.service';
+import { NutsPlatformService } from 'src/app/common/web3/nuts-platform.service';
 import { CurrencyService } from 'src/app/common/currency-select/currency.service';
+import { TokenService } from 'src/app/common/token/token.service';
+import { TokenModel } from 'src/app/common/token/token.model';
+import { AccountService, AccountsBalance } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-dashboard-account-balance',
@@ -16,13 +18,13 @@ import { CurrencyService } from 'src/app/common/currency-select/currency.service
 export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
   private instruments = ['Lending', 'Borrowing', 'Swap', 'Placeholder'];
   private instrumentChineseLabels = ['借款', '贷款', '互换', 'Placeholder'];
-  private assets = ['ETH', 'USDT', 'USDC', 'NUTS', 'DAI', 'Placeholder'];
+  private tokens: TokenModel[] = [];
 
   public totalValue = 0;
   public instrumentValue = [0, 0, 0];
   public instrumentPercentage = [0, 0, 0];
-  public assetValue = [0, 0, 0, 0, 0];
-  public assetPercentage = [0, 0, 0, 0, 0];
+  public tokenValue = [0, 0, 0, 0, 0];
+  public tokenPercentage = [0, 0, 0, 0, 0];
 
   // Instrument Chart
   public instrumentChartLabels: Label[] = this.instruments;;
@@ -64,8 +66,8 @@ export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
   };
 
   // Asset Chart
-  public assetChartLabels: Label[] = this.assets;
-  public assetChartColors = [{
+  public tokenChartLabels: Label[];
+  public tokenChartColors = [{
     backgroundColor: [
       '#627EEA',
       '#53AE94',
@@ -75,10 +77,10 @@ export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
       '#F1EDEA',
     ]
   }];
-  public assetChartData: MultiDataSet = [
+  public tokenChartData: MultiDataSet = [
     [10, 20, 30, 40, 50]
   ];
-  public assetChartOptions = {
+  public tokenChartOptions = {
     title: {
       display: true,
       text: 'Asset Composition',
@@ -106,26 +108,28 @@ export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
   private accountBalancesSubscription: Subscription;
   private currencySubscription: Subscription;
 
-  constructor(private accountBalanceService: AccountBalanceService, private priceOracleService: PriceOracleService,
+  constructor(private accountService: AccountService, private priceOracleService: PriceOracleService,
     private nutsPlatformService: NutsPlatformService, public currencyService: CurrencyService,
-    private zone: NgZone) { }
+    private tokenService: TokenService, private zone: NgZone) { }
 
   ngOnInit() {
+    this.tokens = this.tokenService.tokens.filter(token => token.supportsTransaction);
+    this.tokenChartLabels = this.tokens.map(token => token.tokenSymbol);
     if (environment.language == 'zh') {
       this.instrumentChartLabels = this.instrumentChineseLabels;
       this.instrumentChartOptions.title.text = '产品资产分布';
-      this.assetChartOptions.title.text = '代币资产分布';  
+      this.tokenChartOptions.title.text = '代币资产分布';  
     }
     
-    this.updateAccountBalances(this.accountBalanceService.accountBalances);
-    this.accountBalancesSubscription = this.accountBalanceService.accountBalancesSubject.subscribe(accountBalances => {
+    this.updateAccountBalances();
+    this.accountBalancesSubscription = this.accountService.accountsBalanceSubject.subscribe(_ => {
       this.zone.run(() => {
-        this.updateAccountBalances(accountBalances);
+        this.updateAccountBalances();
       });
     });
     this.currencySubscription = this.currencyService.currencyUpdatedSubject.subscribe(_ => {
       this.zone.run(() => {
-        this.updateAccountBalances(this.accountBalanceService.accountBalances);
+        this.updateAccountBalances();
       });
     });
   }
@@ -135,26 +139,23 @@ export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
     this.currencySubscription.unsubscribe();
   }
 
-  private async updateAccountBalances(userBalance: AccountBalances) {
+  private async updateAccountBalances() {
+    const accountsBalance = this.accountService.accountsBalance;
     const instrumentLabels = environment.language === 'zh' ? this.instrumentChineseLabels : this.instruments;
     const instrumentsValue = [0, 0, 0, 0];
-    const assetsValue = [0, 0, 0, 0, 0, 0];
-    const targetTokenAddress = this.currencyService.currency === 'USD' ? USD_ADDRESS : CNY_ADDRESS;
+    const tokenValue = [0, 0, 0, 0, 0, 0];
     let totalValue = 0;
     // For each instrument
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 5; j++) {
         const instrument = this.instruments[i].toLowerCase();
-        const asset = this.assets[j];
-        if (!userBalance[instrument] || !userBalance[instrument][asset]) continue;
+        if (!accountsBalance[instrument] || !accountsBalance[instrument][this.tokens[j].tokenAddress]) continue;
+        const tokenConvertedValue = await this.priceOracleService.getConvertedCurrencyValue(this.tokens[j],
+          accountsBalance[instrument][this.tokens[j].tokenAddress]);
 
-        const assetAddress = this.nutsPlatformService.getTokenAddressByName(asset);
-        const assetConvertedValue = await this.priceOracleService.getConvertedValue(targetTokenAddress,
-          assetAddress, userBalance[instrument][asset]);
-
-        instrumentsValue[i] += assetConvertedValue;
-        assetsValue[j] += assetConvertedValue;
-        totalValue += assetConvertedValue;
+        instrumentsValue[i] += tokenConvertedValue;
+        tokenValue[j] += tokenConvertedValue;
+        totalValue += tokenConvertedValue;
       }
     }
 
@@ -165,36 +166,36 @@ export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
         this.instrumentPercentage[i] = Math.floor(instrumentsValue[i] * 100 / totalValue);
       }
       for (let j = 0; j < 5; j++) {
-        this.assetPercentage[j] = Math.floor(assetsValue[j] * 100 / totalValue);
+        this.tokenPercentage[j] = Math.floor(tokenValue[j] * 100 / totalValue);
       }
 
       // Update labels
       this.instrumentChartLabels = instrumentLabels.map((value, index) => {
         return `${value}: ${this.instrumentPercentage[index]}%`;
       })
-      this.assetChartLabels = this.assets.map((value, index) => {
-        return `${value}: ${this.assetPercentage[index]}%`;
+      this.tokenChartLabels = this.tokens.map((value, index) => {
+        return `${value.tokenSymbol}: ${this.tokenPercentage[index]}%`;
       });
 
       // Sets charts data
       this.instrumentValue = instrumentsValue;
-      this.assetValue = assetsValue;
+      this.tokenValue = tokenValue;
     } else {
       // Update labels
       this.instrumentChartLabels = instrumentLabels.map((value, index) => {
         return `${value}: _ _%`;
       })
-      this.assetChartLabels = this.assets.map((value, index) => {
-        return `${value}: _ _%`;
+      this.tokenChartLabels = this.tokens.map((value, index) => {
+        return `${value.tokenSymbol}: _ _%`;
       });
 
       // Sets charts data
       this.instrumentValue = [0, 0, 0, 100];
-      this.assetValue = [0, 0, 0, 0, 0, 100];
+      this.tokenValue = [0, 0, 0, 0, 0, 100];
     }
 
     this.instrumentChartData = [this.instrumentValue];
-    this.assetChartData = [this.assetValue];
+    this.tokenChartData = [this.tokenValue];
   }
 
   private getInstrumentTooltip(tooltipItem, data) {
@@ -211,13 +212,9 @@ export class DashboardAccountBalanceComponent implements OnInit, OnDestroy {
   }
 
   private getAssetTooltip(tooltipItem, data) {
-    const asset = this.assets[tooltipItem.index];
+    const token = this.tokens[tooltipItem.index];
     const currency = this.currencyService.getCurrencySymbol();
 
-    if (asset.indexOf("Placeholder") >= 0) {
-      return `${currency} 0`;
-    } else {
-      return `${asset}: ${currency} ${this.assetValue[tooltipItem.index].toFixed(2)}`;
-    }
+    return `${token.tokenSymbol}: ${currency} ${this.tokenValue[tooltipItem.index].toFixed(2)}`;
   }
 }
