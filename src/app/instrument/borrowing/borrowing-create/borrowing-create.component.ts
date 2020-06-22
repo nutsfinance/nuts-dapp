05ebf3cbb77e8659a1508a0620c39c3b22cdd20c
Shengda <dingshengda@gmail.com>
2020-06-22 12:03:15 -0700

    Fix borrowing create

--- src/app/instrument/borrowing/borrowing-create/borrowing-create.component.ts
+++ src/app/instrument/borrowing/borrowing-create/borrowing-create.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { MatButtonToggleChange, MatDialog } from '@angular/material';
 
-import { NutsPlatformService } from '../../../common/web3/nuts-platform.service';
+import { NutsPlatformService, BORROWING_NAME } from '../../../common/web3/nuts-platform.service';
 import { PriceOracleService } from '../../../common/web3/price-oracle.service';
-import { InstrumentService } from 'src/app/common/web3/instrument.service';
 import { TransactionInitiatedDialog } from 'src/app/common/transaction-initiated-dialog/transaction-initiated-dialog.component';
-import { AccountBalanceService } from 'src/app/common/web3/account-balance.service';
+import { TokenModel } from 'src/app/common/token/token.model';
+import { BorrowingService } from '../borrowing.service';
+import { AccountService } from 'src/app/account/account.service';
+import { TokenService } from 'src/app/common/token/token.service';
 
 @Component({
   selector: 'app-borrowing-create',
@@ -14,23 +16,23 @@ import { AccountBalanceService } from 'src/app/common/web3/account-balance.servi
   styleUrls: ['./borrowing-create.component.scss']
 })
 export class BorrowingCreateComponent implements OnInit {
-  private tokens = ["ETH", "USDC", "USDT", "DAI", "NUTS"];
   @ViewChild('form', { static: true }) private form: NgForm;
+  public tokens: TokenModel[] = [];
 
   public createFormGroup: FormGroup;
   public showAlternativeTenor = false;
   public showAlternativeColleral = false;
   public showAlternativeInterest = false;
-  public principalToken = this.tokens[0];
-  public principalValue = 0;
-  public collateralTokenBalance: number;
-  public collateralToken = this.tokens[1];
-  public collateralValue = 0;
-  public interestValue = 0;
+  public principalToken: TokenModel;
+  public principalValue = '0';
+  public collateralTokenBalance = '0';
+  public collateralToken: TokenModel;
+  public collateralValue = '0';
+  public interestValue = '0';
 
-  constructor(private nutsPlatformService: NutsPlatformService, private instrumentService: InstrumentService,
-    private priceOracleSercvice: PriceOracleService, private accountBalanceService: AccountBalanceService,
-    private zone: NgZone, private dialog: MatDialog) { }
+  constructor(private nutsPlatformService: NutsPlatformService, private borrowingService: BorrowingService,
+    private priceOracleSercvice: PriceOracleService, private accountService: AccountService,
+    private tokenService: TokenService, private zone: NgZone, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.createFormGroup = new FormGroup({
@@ -40,27 +42,31 @@ export class BorrowingCreateComponent implements OnInit {
       'interestRate': new FormControl('', this.validInterestRate),
     });
     this.createFormGroup.valueChanges.subscribe(_ => {
-      this.principalValue = this.nutsPlatformService.getTokenActualValueByName(this.principalToken, this.createFormGroup.value['principalAmount']);
-      this.computeCollateralValue();
-      this.computeInterestValue();
+      this.principalValue = this.tokenService.getActualValue(this.principalToken.tokenAddress, this.createFormGroup.value['principalAmount']);
+      this.borrowingService.getCollateralValue(this.principalToken, this.collateralToken, this.principalValue,
+        this.createFormGroup.value['collateralRatio']).then(value => {
+          this.collateralValue = value;
+        });
+      this.interestValue = this.borrowingService.getInterestValue(this.principalValue, this.createFormGroup.value['interestRate'],
+        this.createFormGroup.value['tenor']);
     });
   }
 
-  onPrincipalTokenSelected(token: string) {
+  onPrincipalTokenSelected(tokenAddress: string) {
     // Update principals
-    this.principalToken = token;
+    this.principalToken = this.tokenService.getTokenByAddress(tokenAddress);
     this.createFormGroup.controls['principalAmount'].reset();
     // Update collaterals
-    this.collateralToken = token === this.tokens[0] ? this.tokens[1] : this.tokens[0];
+    this.collateralToken = tokenAddress === this.tokens[0].tokenAddress ? this.tokens[1] : this.tokens[0];
   }
 
   onTenorChange(tenorChange: MatButtonToggleChange) {
     this.createFormGroup.patchValue({ 'tenor': tenorChange.value });
   }
 
-  onCollateralTokenSelected(token: string) {
-    this.collateralToken = token;
-    this.collateralTokenBalance = this.accountBalanceService.getTokenBalance('lending', this.collateralToken);
+  onCollateralTokenSelected(tokenAddress: string) {
+    this.collateralToken = this.tokenService.getTokenByAddress(tokenAddress);
+    this.collateralTokenBalance = this.accountService.getAccountBalance(BORROWING_NAME, this.collateralToken.tokenAddress);
     this.createFormGroup.controls['collateralRatio'].reset();
   }
 
@@ -72,25 +78,11 @@ export class BorrowingCreateComponent implements OnInit {
     this.createFormGroup.patchValue({ 'interestRate': interestRateChange.value });
   }
 
-  computeCollateralValue() {
-    const principalTokenAddress = this.nutsPlatformService.getTokenAddressByName(this.principalToken);
-    const collateralTokenAddress = this.nutsPlatformService.getTokenAddressByName(this.collateralToken);
-    if (!this.principalValue) {
-      this.collateralValue = 0;
-      return;
-    }
-
-    this.priceOracleSercvice.getConvertedValue(collateralTokenAddress, principalTokenAddress,
-      this.principalValue * this.createFormGroup.value['collateralRatio'], 100).then(value => {
-        this.collateralValue = value;
-      });
-  }
-
   async createBorrowingIssuance() {
     if (!this.createFormGroup.valid || this.collateralTokenBalance < this.collateralValue) {
       return;
     }
-    this.instrumentService.createBorrowingIssuance(this.principalToken, this.principalValue,
+    this.borrowingService.createBorrowingIssuance(this.principalToken, this.principalValue,
       this.collateralToken, this.createFormGroup.value['collateralRatio'], this.createFormGroup.value['tenor'],
       this.createFormGroup.value['interestRate'])
       .on('transactionHash', transactionHash => {
@@ -100,10 +92,7 @@ export class BorrowingCreateComponent implements OnInit {
           const transactionInitiatedDialog = this.dialog.open(TransactionInitiatedDialog, {
             width: '90%',
             data: {
-              type: 'create_issuance',
-              instrument: 'borrowing',
-              tokenAmount: this.principalValue,
-              tokenName: this.principalToken,
+              type: 'create_issuance', instrument: BORROWING_NAME, tokenAmount: this.principalValue, tokenName: this.principalToken.tokenSymbol,
             },
           });
           transactionInitiatedDialog.afterClosed().subscribe(() => {
@@ -112,40 +101,13 @@ export class BorrowingCreateComponent implements OnInit {
             document.body.scrollTop = document.documentElement.scrollTop = 0;
           });
         });
-      
-        this.monitorBorrowingTransaction(transactionHash);  
       });
-  }
-
-  private monitorBorrowingTransaction(transactionHash) {
-    // Monitoring transaction status(work around for Metamask mobile)
-    const interval = setInterval(async () => {
-      const receipt = await this.nutsPlatformService.web3.eth.getTransactionReceipt(transactionHash);
-      if (!receipt || !receipt.blockNumber) return;
-
-      console.log('Create receipt', receipt);
-      // New borrowing issuance created. Need to refresh the borrowing issuance list.
-      this.instrumentService.reloadBorrowingIssuances(5, 3000);
-      // New borrowing issuance created. Need to update the principal balance as well.
-      this.accountBalanceService.getUserBalanceFromBackend(5, 3000);
-      this.nutsPlatformService.transactionConfirmedSubject.next(receipt.transactionHash);
-      clearInterval(interval);
-    }, 2000);
   }
 
   resetForm() {
     this.principalToken = this.tokens[0];
     this.collateralToken = this.tokens[1];
     this.form.resetForm();
-  }
-
-  computeInterestValue() {
-    const principalAmount = Math.floor(this.createFormGroup.value['principalAmount'] * 10000);
-    const interestRate = Math.floor(this.createFormGroup.value['interestRate'] * 10000);
-    const tenor = this.createFormGroup.value['tenor'];
-    const interest = principalAmount * interestRate * tenor / 10000000000;
-
-    this.interestValue = interest;
   }
 
   /**
@@ -166,11 +128,8 @@ export class BorrowingCreateComponent implements OnInit {
     if (!control.value) {
       return { 'required': true };
     }
-    if ((this.principalToken === 'ETH' && Number.isNaN(Number(control.value))) || Number(control.value) <= 0) {
+    if (Number.isNaN(Number(control.value)) || Number(control.value) <= 0) {
       return { 'nonPositiveAmount': true };
-    }
-    if (this.principalToken !== 'ETH' && !/^[1-9][0-9]*$/.test(control.value)) {
-      return { 'nonIntegerAmount': true };
     }
     return null;
   }
