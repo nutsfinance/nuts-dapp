8c230c02c7c9618d4d717ed24ab9615faba72fb0
Shengda <dingshengda@gmail.com>
2020-06-22 12:06:44 -0700

    Fix borrowing card

--- src/app/instrument/borrowing/borrowing-card/borrowing-card.component.ts
+++ src/app/instrument/borrowing/borrowing-card/borrowing-card.component.ts
@@ -2,13 +2,14 @@ import { Component, Input, OnInit, OnDestroy, NgZone } from '@angular/core';
 
 import { environment } from '../../../../environments/environment';
 import { IssuanceModel } from '../../issuance.model';
-import { NutsPlatformService, USD_ADDRESS, CNY_ADDRESS } from 'src/app/common/web3/nuts-platform.service';
+import { NutsPlatformService } from 'src/app/common/web3/nuts-platform.service';
 import { PriceOracleService } from 'src/app/common/web3/price-oracle.service';
 import { Subscription } from 'rxjs';
 import { CurrencyService } from 'src/app/common/currency-select/currency.service';
 import { BorrowingIssuanceModel } from '../borrowing-issuance.model';
 import { TokenModel } from 'src/app/common/token/token.model';
 import { TokenService } from 'src/app/common/token/token.service';
+import { BorrowingService } from '../borrowing.service';
 
 @Component({
   selector: 'app-borrowing-card',
@@ -19,10 +20,9 @@ export class BorrowingCardComponent implements OnInit, OnDestroy {
   @Input() public issuance: IssuanceModel;
   public borrowingIssuance: BorrowingIssuanceModel;
   public language = environment.language;
-  public currentAccount: string;
   public borrowingToken: TokenModel;
   public collateralToken: TokenModel;
-  public collateralValue = 0;
+  public collateralValue = '0';
 
   public convertedBorrowingValue: Promise<number>;
   public convertedCollateralValue: Promise<number>;
@@ -33,17 +33,15 @@ export class BorrowingCardComponent implements OnInit, OnDestroy {
   private currencyUpdatedSubscription: Subscription;
 
   constructor(public nutsPlatformService: NutsPlatformService, private priceOracleService: PriceOracleService,
-    private tokenService: TokenService, public currencyService: CurrencyService, private zone: NgZone) { }
+    private borrowingService: BorrowingService, private tokenService: TokenService, public currencyService: CurrencyService, private zone: NgZone) { }
 
   ngOnInit() {
-    this.currentAccount = this.nutsPlatformService.currentAccount;
     this.borrowingIssuance = this.issuance.issuancecustomproperty as BorrowingIssuanceModel;
     this.borrowingToken = this.tokenService.getTokenByAddress(this.borrowingIssuance.borrowingtokenaddress);
     this.collateralToken = this.tokenService.getTokenByAddress(this.borrowingIssuance.collateraltokenaddress);
     // If the collateral value is not set
-    if (this.borrowingIssuance.collateralamount == 0) {
-      const collateralDisplayValue = this.tokenService.getDisplayValue
-      this.priceOracleService.getConvertedValue(this.issuance.collateralTokenAddress, this.issuance.borrowingTokenAddress, this.issuance.borrowingAmount * this.issuance.collateralRatio, 10000).then(value => {
+    if (this.borrowingIssuance.collateralamount === '0') {
+      this.borrowingService.getBorrowingCollateralValue(this.borrowingIssuance).then(value => {
         this.collateralValue = value;
       });
     } else {
@@ -62,15 +60,13 @@ export class BorrowingCardComponent implements OnInit, OnDestroy {
   }
 
   private updateConvertedValues() {
-    const targetTokenAddress = this.currencyService.currency === 'USD' ? USD_ADDRESS : CNY_ADDRESS;
-    this.convertedCollateralValue = this.priceOracleService.getConvertedValue(targetTokenAddress,
-      this.issuance.borrowingTokenAddress, this.issuance.borrowingAmount * this.issuance.collateralRatio, 10000);
-    this.convertedBorrowingValue = this.priceOracleService.getConvertedValue(targetTokenAddress,
-      this.issuance.borrowingTokenAddress, this.issuance.borrowingAmount);
-    this.convertedPerDayInterestValue = this.priceOracleService.getConvertedValue(targetTokenAddress,
-      this.issuance.borrowingTokenAddress, this.issuance.borrowingAmount * this.issuance.interestRate, 1000000);
-    this.convertedTotalInterestValue = this.priceOracleService.getConvertedValue(targetTokenAddress,
-      this.issuance.borrowingTokenAddress, this.issuance.borrowingAmount * this.issuance.interestRate * this.issuance.tenorDays, 1000000);
+    // Compute converted issuance token values
+    this.convertedCollateralValue = this.priceOracleService.getConvertedCurrencyValue(this.collateralToken, this.collateralValue);
+    this.convertedBorrowingValue = this.priceOracleService.getConvertedCurrencyValue(this.borrowingToken, this.borrowingIssuance.borrowingamount);
+    this.convertedPerDayInterestValue = this.priceOracleService.getConvertedCurrencyValue(this.borrowingToken,
+      this.borrowingService.getPerDayInterest(this.borrowingIssuance));
+    this.convertedTotalInterestValue = this.priceOracleService.getConvertedCurrencyValue(this.borrowingToken,
+      this.borrowingIssuance.interestamount);
   }
 
 }
